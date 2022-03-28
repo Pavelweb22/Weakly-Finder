0c0ef4a3ad47cc52ad6845057f9d49cfd7b4a202
Pavelweb22 <pavelweb2016@yandex.ru>
2022-03-28 19:51:56 +0300

    Initial Commit

---
+++ Weekly Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weekly Finder
+//
+//  Created by Павел Яковенко on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
